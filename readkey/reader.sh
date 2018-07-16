550e68f6eb8e1cba456afe397c2a3ad396c87971
EC2 Default User <ec2-user@ip-172-31-9-125.eu-central-1.compute.internal>
2018-07-16 09:17:18 +0000

    fixed readkey silent mode

--- readkey/reader.sh
+++ readkey/reader.sh
@@ -2,7 +2,7 @@ mkdir /tmp/rfids/
 while read line
 do
   touch "/tmp/rfids/$line"
-  wget "http://www.mbd.hu/fundamenta/storeCoin.php?rfid=$line" > /tmp/last_coin_store 2>&1  &
+  wget -qO-  http://www.mbd.hu/fundamenta/storeCoin.php?rfid=$line &> /tmp/last_coin_store 
   echo "$line"
 done < "${1:-/dev/stdin}"
 
