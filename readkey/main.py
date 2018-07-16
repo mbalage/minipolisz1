468bfbc118a5ea6c9ad9c479ae4261a69999f8d9
EC2 Default User <ec2-user@ip-172-31-9-125.eu-central-1.compute.internal>
2018-07-16 11:56:48 +0000

    python script and logo

--- readkey/main.py
+++ readkey/main.py
@@ -8,9 +8,11 @@ import pprint
 
 
 
+sum = 0;
+
 pp = pprint.PrettyPrinter(indent=4)
 
-values = dict()
+
 
 
 # a cronjob hopefully keeps values.json synched from mbd.hu
@@ -23,10 +25,8 @@ def read_values():
           if (len(v)==0):
               print "values.json: nincs benne adat! "
           else:     
-              values = v 
-              print "Mostantol alkalmazott RFID<->ertek parosok: (osszesen "+str(len(values))+" darab):"
-              pp.pprint (values)
-              return
+              #values = v 
+              return v
           
     except:
        print "Hiba a json file megnyitasanal... "
@@ -38,41 +38,73 @@ def read_values():
        exit()
           
           
+# hol tartottunk legutobb?
+def read_sum():      
+    try: 
+       os.system (" wget -O lastsum.json  http://mbd.hu/fundamenta/getSum.php")         
+       with open('lastsum.json') as f:
+          v = json.load(f)
+          pp.pprint(v)
+          return int(v.get("sum", 33))
+    except:
+       print  "Internal error "+traceback.format_exc()
+       return 0
            
-        
-        
+
+# sum elmentese, biztos ami biztos
+def write_sum(sum):      
+    try: 
+       os.system (" wget -O lastsetsum.json http://mbd.hu/fundamenta/setSum.php?newsum="+str(sum))         
+    except:
+       print  "Internal error "+traceback.format_exc()
     
     
 print "Coin 1.0 ***"
 
 # cronjob is frissiti majd, de biztos ami biztos kezdjuk ezzel
 os.system (" wget -O values.json  http://mbd.hu/fundamenta/getValues.php")
+sum=read_sum()
+print "Ezzel SUM-mal indulunk:"+str(sum)
 
-read_values()
- 
 
+# RFID regex
 p = re.compile ('[0-9a-fA-F]{10}') 
 
 while True:   
+
+    values = read_values()
+    print "Mostantol alkalmazott RFID<->ertek parosok: (osszesen "+str(len(values))+" darab):"
+    pp.pprint (values)
+    print "Varakozas RFID-re..."
+
     line = (sys.stdin.readline()).rstrip()
     if (p.match(line)): 
         print ("RFID '"+line+"'")
     else:
-        print ("nem RFID input '"+line+"'")
+        print ("Nem RFID input '"+line+"'")
         continue
         
-    
-    if line in values:
-        try:
-            v = values.get(line)
-            val = int(v)
-        except:
-            print  "Internal error "+traceback.format_exc()
+ 
+    pp.pprint (values)
+    try:   
+        v = values.get(line)
+        val = int(v, 0)
+        if (val == 0):
+            print "Ismeretlen RFID, nincs hozzarendelt erme!"            
+            continue
+        print "RFID-hez rendelt erme erteke: "+str(val)     
+        
+        sum += val
+        
+        print "Az uj SUM: "+str(sum)
+        write_sum (sum) 
+        
+        print ()
+        
+              
             
-               
-        print "RFID erteke: "+str(val)
-    else:
-        print "Ismeretlen RFID, nincs hozzarendelt erme!"
+    except:
+        print  "Internal error "+traceback.format_exc()
         
                 
 
